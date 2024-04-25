14e4c10111e978141bde137c72df7a8201ff3df6
Mohamed Fouad <m.elsayedfouad99@gmail.com>
2024-04-25 02:56:26 +0200

    api_key

--- lib/constant/api_consts.dart
+++ lib/constant/api_consts.dart
@@ -4,3 +4,4 @@
 // Last Modified:24/April/2024
 // Description:This is a class that defines the API constants for the application.
 String BASE_URL = "https://api.openai.com/v1";
+String API_KEY = "sk-proj-U9GVux3CiCqMwjEfCvLRT3BlbkFJwj9GINyHME9FdAK4eatt";
